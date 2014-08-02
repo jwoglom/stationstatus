3a43be894c5c5f648acf2455ca9fa2fd355202c6
James Woglom <j@wogloms.net>
2014-08-02 19:42:14 -0400

    update utils

--- utils/mkblob.php
+++ utils/mkblob.php
@@ -3,10 +3,10 @@ $d = array();
 rename("../data/blob.txt", "../data/blob.txt.".time());
 $base = "http://127.0.0.1/metro/";
 // Stations and Lines
-
+$key = trim(file_get_contents("../data/apikey.txt"));
 if(isset($argv, $argv[1]) && $argv[1] == "all") {
-    $d["lines"] = json_decode(file_get_contents($base."query.php?path=Rail.svc/json/jLines&api_key=***REMOVED***&format=json"));
-    $d["lines"] = json_decode(file_get_contents($base."query.php?path=Rail.s    vc/json/jStations&api_key=***REMOVED***&format=json"));
+    $d["lines"] = json_decode(file_get_contents($base."query.php?path=Rail.svc/json/jLines&api_key=".$key."&format=json"));
+    $d["lines"] = json_decode(file_get_contents($base."query.php?path=Rail.s    vc/json/jStations&api_key=".$key."&format=json"));
 } else {
     $d["lines"] = json_decode(file_get_contents("../data/lines.txt"))->Lines;
     $d["stations"] = json_decode(file_get_contents("../data/stations.txt"))->Stations;
@@ -22,7 +22,7 @@ $d["coords"] = json_decode(file_get_contents("../data/coords.txt"));
 $ids = array("K08","D13","A15","B11","J03","G05","C15","E06","F11","E10","N06","G05");
 
 for($i=0; $i<sizeof($ids); $i++) {
-    $d["path-".$ids[$i]."-".$ids[$i+1]] = json_decode(file_get_contents($base."query.php?path=Rail.svc/json/jPath&api_key=***REMOVED***&format=json&FromStationCode=".$ids[$i]."&ToStationCode=".$ids[$i+1]))->Path;
+    $d["path-".$ids[$i]."-".$ids[$i+1]] = json_decode(file_get_contents($base."query.php?path=Rail.svc/json/jPath&api_key=".$key."&format=json&FromStationCode=".$ids[$i]."&ToStationCode=".$ids[$i+1]))->Path;
     $i++;
 }
 
