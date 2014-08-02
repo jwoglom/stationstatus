beb308cdde236a26b95277247a852020a09e09d8
James Woglom <j@wogloms.net>
2014-08-02 13:48:15 -0400

    external js

---
+++ ui.station.js
@@ -0,0 +1,119 @@
+station = {
+    id: null,
+    st: null,
+    init: function() {
+        if(location.hash.indexOf('station=') != -1) {
+            this.id = location.hash.split('station=')[1];
+            this.id = this.id.split('&')[0];
+            this.st = metro.stations[this.id];
+            if(typeof this.st == 'undefined') return;
+            this.add();
+        }
+        $(window).on("hashchange", function() {
+            location.reload();
+        });
+    },
+    add: function() {
+        this.showInformation();
+        this.getPrediction();
+
+        $(".loading").hide();
+    },
+    showInformation: function() {
+        $(".header .title").html(this.st.name+" Station");
+        this.showColorbar();
+        var url = geo.getMapsURL(this.st.coords.lat, this.st.coords.long, 15);
+        $(".contents .stationinfo").attr("style", "background-image: url('" + url + "')");
+        assign(".stationinfo .bottom-button", "https://maps.google.com/maps?q="+this.st.coords.lat+", "+this.st.coords.long);
+        this.showOrder();
+    },
+    showOrder: function() {
+        $tpl = $(".contents .card-template.order");
+        for(line in this.st.next) {
+            $l = $tpl.clone();
+            var nxt = this.st.next[line], en;
+            if(nxt) {
+                $(".half-bottom-button .right", $l).attr("data-station", nxt.code).html(nxt.name);
+                $(".half-bottom-button .right", $l).addClass(line.toLowerCase());
+                en = metro.stations[metro.lines[line].codes.end];
+                $(".toward .right", $l).html(en.name);
+            } else {
+                $(".right", $l).css("visibility", "hidden");
+            }
+            var prv = this.st.prev[line], st;
+            if(prv) {
+                $(".half-bottom-button .left", $l).attr("data-station", prv.code).html(prv.name);
+                $(".half-bottom-button .left", $l).addClass(line.toLowerCase());
+                st = metro.stations[metro.lines[line].codes.start];
+                $(".toward .left", $l).html(st.name);
+            } else {
+                $(".left", $l).css("visibility", "hidden");
+            }
+
+            console.debug(line+" prev: "+prv+" next: "+nxt);
+            console.debug(line+" starts: "+st+" ends: "+en);
+            $l.removeClass("card-template").css("display","");
+            $l.addClass("card");
+            $tpl.after($l);
+        }
+
+        $(".half-bottom-button > div").click(function() {
+            setTimeout(function() {
+                location.href = "ui-station.html#station=" + $(this).attr("data-station");
+            }.bind(this), 400);
+        });
+    },
+    showColorbar: function() {
+        for(var i=0; i<this.st.totalLines.length; i++) {
+            $(".container .multibackground").append("<span style='background-color: #"+metro.colors[this.st.totalLines[i].code]+"; opacity: 1; width: "+(100 / this.st.totalLines.length)+"%'></span>");
+        }
+    },
+    getPrediction: function() {
+        WMATA.gettiming(function() {
+            var json = WMATA.timing["Trains"];
+            console.log(json);
+            var nm = 0;
+            for(num in json) {
+                if(json[num].LocationCode.toLowerCase().trim() == station.st.code.toLowerCase()) {
+                    console.log(json[num]);
+                    station.addPrediction(json[num]);
+                    nm++;
+                }
+            }
+            console.debug("Of "+num+" trains, "+nm+" are at "+station.st);
+            if(nm <= 1) {
+                $(".card.nexttrains table tbody").append("<tr><td colspan=5><center style='font-size: 14px'>There are no trains scheduled to arrive at this station.</center></td></tr>");
+            }
+        });
+    },
+    addPrediction: function(train) {
+        $c = $(".card.nexttrains table tbody");
+        var mins = min = train.Min;
+        if(mins == "ARR") mins = "<span class='arr'>Arriving</span>";
+        else if(mins == "BRD") mins = "<span class='brd'>Boarding</span>";
+        
+        var tm = parseInt(+new Date);
+        if(min != "ARR" && min != "BRD") {
+            tm += 60 * parseInt(min) * 1000;
+        }
+        var time = new Date(tm),
+        h = (time.getHours() > 12 ? time.getHours() - 12 : time.getHours()),
+        m = (time.getMinutes()+1),
+        timel = h+":"+(m<10 ? "0"+m : m);
+        var str = '<tr>\n' +
+                  '<td class="line"><span class="line ' + train.Line.toLowerCase() + '">' + train.Line + '</span></td>\n' +
+                  '<td class="num">' + (train.Car ? train.Car : '-') + '</td>\n' +
+                  '<td class="stop">' + train.DestinationName + '</td>' +
+                  '<td class="mins">' + mins + '</td>\n' +
+                  '<td class="time">' + timel + '</td>\n' +
+                  '</tr>';
+        $c.append(str);
+    },
+    click: function() { // binded: this=object
+        var st = $(this).attr("data-station");
+        console.info("CLICK: "+st);
+        setTimeout(function() {
+            location.href = "ui-station.html#id=" + st;
+        }, 400);
+    }
+};
---
+++ ui.trains.js
@@ -0,0 +1,120 @@
+trains = {
+    line: false,
+    init: function() {
+        if(location.hash.indexOf('line=') != -1) {
+            this.line = location.hash.split('line=')[1];
+            this.line = this.line.split('&')[0];
+            this.line = this.line.toUpperCase();
+            $(".header .title").html(metro.lines[this.line].name+" Line");
+            $(".header").css("background-color", "#"+metro.colors[this.line]);
+            if(~$.inArray(this.line, metro.colorsBlack)) {
+                $(".header .title").css("color", "black");
+            }
+        }
+        this.getPrediction();
+        $(".loading").hide();
+        $(window).on("hashchange", function() {
+            location.reload();
+        });
+    },
+    getPrediction: function() {
+        WMATA.gettiming(function() {
+            var json = WMATA.timing["Trains"];
+            console.log(json);
+            var nm = 0;
+            stdata = {};
+            for(num in json) {
+                var d = json[num];
+                if(typeof stdata[d.LocationCode] == 'undefined') {
+                    stdata[d.LocationCode] = [];
+                }
+                stdata[d.LocationCode].push(d);
+            }
+            var ltrs = {};
+            for(stcode in stdata) {
+                var cd = stcode.trim().toUpperCase();
+                var trns = stdata[cd];
+                var stn = metro.stations[cd];
+                if(trains.line != false && stn.totalLines.indexOf(metro.lines[trains.line]) == -1) {
+                    /* Skip if not in line */
+                    console.debug("Showing only line "+trains.line+", not in "+stn.totalLines.join(',')+" at "+stn);
+                    continue;
+                }
+                console.info("Of "+num+" trains, "+trns.length+" are at "+stn);
+                if(trains.line == false) {
+                    // Showing all trains
+                    trains.doStation(trns, cd);
+                } else {
+                    ltrs[stcode] = trns;
+                }
+            }
+            if(trains.line != false) {
+                // Show line in order
+                console.log(ltrs);
+                var ln = metro.lines[trains.line];
+                for(var i=0; i<ln.route.length; i++) {
+                    var s = ln.route[i];
+                    console.log(s, ltrs[s.code]);
+                    if(typeof ltrs[s.code] != 'undefined') {
+                        trains.doStation(ltrs[s.code], s.code);
+                    }
+                }
+            }
+        });
+    },
+    doStation: function(trns, cd) {
+        var stn = metro.stations[cd];
+        if(trns.length > 0) {
+            trains.addStation(stn);
+        }
+        for(var i=0; i<trns.length; i++) {
+            trains.addPrediction(trns[i], cd);
+            if(i >= 2) {
+                var ch = parseInt($(".card.nexttrains."+cd).css("height").split("px")[0]);
+                $(".card.nexttrains."+cd).css(
+                    "height", ch + 30
+                )
+            }
+        }
+    },
+    addStation: function(st) {
+        var str = '<div class="card nexttrains ' + st.code + '">\n' +
+                  '<div class="title">' + st.name + '</div>' +
+                  '<table><thead><tr>' +
+                  '<th>Line</th><th>Cars</th><th>Destination</th><th>Mins</th><th>Arriving At</th>' +
+                  '</tr></thead>' +
+                  '<tbody></tbody>' +
+                  '</table></div>';
+        $(".contents").append(str);
+    },
+    addPrediction: function(train, st) {
+        $c = $(".card.nexttrains."+st+" table tbody");
+        var mins = min = train.Min;
+        if(mins == "ARR") mins = "<span class='arr'>Arriving</span>";
+        else if(mins == "BRD") mins = "<span class='brd'>Boarding</span>";
+        
+        var tm = parseInt(+new Date);
+        if(min != "ARR" && min != "BRD") {
+            tm += 60 * parseInt(min) * 1000;
+        }
+        var time = new Date(tm),
+        h = (time.getHours() > 12 ? time.getHours() - 12 : time.getHours()),
+        m = (time.getMinutes()+1),
+        timel = h+":"+(m<10 ? "0"+m : m);
+        var str = '<tr>\n' +
+                  '<td class="line"><span class="line ' + train.Line.toLowerCase() + '">' + train.Line + '</span></td>\n' +
+                  '<td class="num">' + (train.Car ? train.Car : '-') + '</td>\n' +
+                  '<td class="stop">' + train.DestinationName + '</td>' +
+                  '<td class="mins">' + mins + '</td>\n' +
+                  '<td class="time">' + timel + '</td>\n' +
+                  '</tr>';
+        $c.append(str);
+    },
+    click: function() { // binded: this=object
+        var st = $(this).attr("data-station");
+        console.info("CLICK: "+st);
+        setTimeout(function() {
+            location.href = "ui-station.html#id=" + st;
+        }, 400);
+    }
+};
