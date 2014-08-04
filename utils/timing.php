a54061f5a2227d213e0d8cc3e1b693a242de953d
James Woglom <j@wogloms.net>
2014-08-04 13:27:36 -0400

    fix scripts

--- utils/timing.php
+++ utils/timing.php
@@ -2,8 +2,8 @@
 
 // Start and end points of each line
 
-$key = trim(file_get_contents("../data/apikey.txt"));
+$key = trim(file_get_contents("data/apikey.txt"));
 $get = file_get_contents("http://api.wmata.com/StationPrediction.svc/json/GetPrediction/All?api_key=".$key);
 $data = json_decode($get);
 $data->timestamp = time();
-file_put_contents("../data/timing.txt", json_encode($data));
+file_put_contents("data/timing.txt", json_encode($data));
