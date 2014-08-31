8019fc018ba20a0e5bf6f5bfcc4de6c3a056563b
James Woglom <j@wogloms.net>
2014-08-31 14:29:24 -0400

    add outages to dynamic

--- data/dynamic.php
+++ data/dynamic.php
@@ -4,9 +4,12 @@ $t = json_decode(file_get_contents("timing.txt"));
 $data["timing"] = $t->Trains;
 $i = json_decode(file_get_contents("incidents.txt"));
 $data["incidents"] = $i->Incidents;
+$o = json_decode(file_get_contents("outages.txt"));
+$data["outages"] = $o->ElevatorIncidents;
 $data["timestamp"] = array(
     "timing" => $t->timestamp,
     "incidents" => $i->timestamp,
+    "outages" => $o->timestamp,
     "dynamic" => time()
 );
 echo json_encode($data);
