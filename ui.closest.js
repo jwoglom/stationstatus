f26bbfd707e24598a9efe23cb2eb879c3b366667
James Woglom <j@wogloms.net>
2014-09-11 00:18:14 -0400

    outages button assignment

--- ui.closest.js
+++ ui.closest.js
@@ -145,4 +145,5 @@ doAssign = function() {
     }
     assign(".card.trip > .bottom-button", "ui-tripselect.html");
     assign(".card.trains > .bottom-button", "ui-trains.html");
+    assign(".card.homeoutages > .bottom-button", "ui-outages.html");
 }
